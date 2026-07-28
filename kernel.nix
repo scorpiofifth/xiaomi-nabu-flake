462d0f08beb0d64711d07fc65b84980aaa297f81
ScorpioFifth <scorpiofifth@qq.com>
2026-07-28 09:15:54 +0800

    feat: use new build method

---
+++ kernel.nix
@@ -0,0 +1,71 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  flex,
+  bison,
+  perl,
+  bc,
+  openssl,
+  elfutils,
+  pahole,
+  dtc,
+  ubootTools,
+  kmod,
+  cpio,
+  gzip,
+  lz4,
+  xz,
+  zlib,
+}:
+stdenv.mkDerivation {
+  pname = "linux-nabu";
+  version = "6.16.0";
+
+  src = fetchurl {
+    url = "https://www.kernel.org/pub/linux/kernel/v6.x/linux-6.16.tar.xz";
+    sha256 = "1a4be2fe6b5246aa4ac8987a8a4af34c42a8dd7d08b46ab48516bcc1befbcd83";
+  };
+
+  nativeBuildInputs = [
+    flex
+    bison
+    perl
+    bc
+    openssl
+    elfutils
+    pahole
+    dtc
+    ubootTools
+    kmod
+    cpio
+    gzip
+    lz4
+    xz
+    zlib
+  ];
+
+  configurePhase = ''
+    echo "-3" > localversion.10-pkgrel
+    echo "-nabu" > localversion.20-pkgname
+
+    cp ${./kernel/config} ./.config
+    make olddefconfig
+  '';
+
+  buildPhase = ''
+    make prepare
+    make -s kernelrelease > version
+    make -j$(nproc) Image Image.gz modules
+    make -j$(nproc) DTC_FLAGS="-@" dtbs
+  '';
+
+  enableParallelBuilding = true;
+
+  meta = with lib; {
+    description = "The Linux Kernel - AArch64 Xiaomi Pad 5";
+    homepage = "https://www.kernel.org/";
+    license = licenses.gpl2;
+    platforms = platforms.aarch64;
+  };
+}
