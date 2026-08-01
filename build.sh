af65a486d452bbf77a6cb4c9cb7da268c2f343fe
ScorpioFifth <scorpiofifth@qq.com>
2026-08-01 15:31:30 +0800

    chore: add more settings

--- build.sh
+++ build.sh
@@ -66,6 +66,11 @@ NIXOS_INSTALL_BOOTLOADER=1 sudo env "PATH=$PATH" nixos-enter \
   -- /nix/var/nix/profiles/system/bin/switch-to-configuration boot
 echo "::endgroup::"
 
+echo "::group::inject config files"
+sudo mkdir -p "$mountPoint"/etc/nixos
+sudo cp "$configFile" "$hardwareFile" "$mountPoint"/etc/nixos
+echo "::endgroup::"
+
 echo "::group::collect efi files"
 mkdir -p "$out/efi"
 sudo cp -r "$mountPoint"/boot/* "$out/efi"
--- image-builder/nixos/configuration.nix
+++ image-builder/nixos/configuration.nix
@@ -1,6 +1,19 @@
 { ... }:
 {
   system.stateVersion = "26.11";
+
   users.users.root.password = "root";
-  boot.initrd.systemd.emergencyAccess = true;
+
+  networking = {
+    enableIPv6 = false;
+    firewall.enable = false;
+    networkmanager.enable = true;
+  };
+
+  services.openssh.enable = true;
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 }
