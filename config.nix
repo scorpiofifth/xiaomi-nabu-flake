672b040538bb212b98023c03eff2ee2f8a2fa798
ScorpioFifth <scorpiofifth@qq.com>
2026-08-01 13:05:47 +0800

    chore: rename files

--- config.nix
+++ config.nix
@@ -8,6 +8,7 @@
     extraGroups = [ "wheel" ];
     initialPassword = "nix";
   };
+
   # TODO: enable it for tests
   boot.initrd.systemd.emergencyAccess = true;
   systemd.enableEmergencyMode = true;
