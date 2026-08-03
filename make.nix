88e1568f36e4d4e479ad46bfb4b3d71ba44a13b9
ScorpioFifth <scorpiofifth@qq.com>
2026-08-03 12:43:12 +0800

    chore: update

--- make.nix
+++ make.nix
@@ -4,6 +4,23 @@
   config,
 }:
 let
+  binPath = lib.makeBinPath (
+    with pkgs;
+    [
+      config.system.build.nixos-install
+      e2fsprogs
+      gptfdisk
+      lkl
+      nix
+      nixos-enter
+      systemdMinimal
+      util-linux
+    ]
+    ++ stdenv.initialPath
+  );
+
+  configBuild = config.system.build.toplevel;
+
   channelSources = pkgs.runCommand "nixos-${config.system.nixos.version}" { } ''
     mkdir -p $out
     cp -prd ${(lib.cleanSource pkgs.path).outPath} $out/nixos
@@ -14,21 +31,13 @@ let
     rm -rf $out/nixos/.git
     echo -n ${config.system.nixos.versionSuffix} > $out/nixos/.version-suffix
   '';
-  binPath = lib.makeBinPath (
-    with pkgs;
-    [
-      util-linux
-      parted
-      e2fsprogs
-      lkl
-      config.system.build.nixos-install
-      nixos-enter
-      nix
-      gptfdisk
-      systemdMinimal
-    ]
-    ++ stdenv.initialPath
-  );
+
+  closureInfo = pkgs.closureInfo {
+    rootPaths = [
+      configBuild
+      channelSources
+    ];
+  };
 in
 pkgs.runCommand "nixos-rootfs" { } ''
   chmod 755 "$TMPDIR"
@@ -38,24 +47,16 @@ pkgs.runCommand "nixos-rootfs" { } ''
   export NIX_STATE_DIR=$TMPDIR/state
 
   tmpRoot="$TMPDIR/root"
-  diskImage="$TMPDIR/nixos.img"
+  diskImage="$TMPDIR/rootfs.img"
 
   mkdir -p $out $tmpRoot
 
-  nix-store --load-db <${
-    pkgs.closureInfo {
-      rootPaths = [
-        config.system.build.toplevel
-        channelSources
-      ];
-    }
-  }/registration
-
+  nix-store --load-db <${closureInfo}/registration
   nixos-install \
     --root $tmpRoot \
     --no-bootloader \
     --no-root-passwd \
-    --system ${config.system.build.toplevel} \
+    --system ${configBuild} \
     --channel ${channelSources} \
     --substituters ""
 
@@ -63,6 +64,7 @@ pkgs.runCommand "nixos-rootfs" { } ''
   mkfs.ext4 $diskImage 
 
   cptofs -p -t ext4 -i $diskImage $tmpRoot/* /
+
   e2fsck -f $diskImage -y
   resize2fs $diskImage -M
 
