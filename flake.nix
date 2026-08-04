73efb0fa50e381a7ed59160f6140a2367305a800
ScorpioFifth <scorpiofifth@qq.com>
2026-08-04 22:27:44 +0800

    chore: update

--- flake.nix
+++ flake.nix
@@ -2,12 +2,13 @@
   description = "A flake for Xiaomi Pad 5(nabu)";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs =
-    { nixpkgs }:
+    { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.aarch64-linux;
     in
     {
       packages.aarch64-linux = {
+        default = self.packages.aarch64-linux.linux-nabu;
         linux-nabu = pkgs.callPackage ./packages/linux-nabu { };
         alsa-ucm-conf-xiaomi-nabu = pkgs.callPackage ./packages/alsa-ucm-conf-xiaomi-nabu { };
         linux-firmware-xiaomi-nabu = pkgs.callPackage ./packages/linux-firmware-xiaomi-nabu { };
