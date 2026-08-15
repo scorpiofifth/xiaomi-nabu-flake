b87ad1b95899aaf863606ae469b082144c6a391d
ScorpioFifth <scorpiofifth@qq.com>
2026-08-15 22:28:56 +0800

    feat: add cachix modules

---
+++ modules/cachix/default.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.nabu;
+in
+{
+  options.nabu.cachix = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
+    url = lib.mkOption {
+      type = lib.types.str;
+      default = "https://nixpkgs-for-nabu.cachix.org";
+    };
+    key = lib.mkOption {
+      type = lib.types.str;
+      default = "nixpkgs-for-nabu.cachix.org-1:OAXPmcIw5ewZYJK9QDLRNJZYy05/uBsNoZIKW7BiKAQ=";
+    };
+  };
+
+  config = lib.mkIf (cfg.enable && cfg.cachix.enable) {
+    nix = {
+      substituters = [ cfg.cachix.url ];
+      trusted-public-keys = [ cfg.cachix.key ];
+    };
+  };
+}
--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,7 @@
 { lib, ... }:
 {
   imports = [
+    ./cachix
     ./firmware
     ./firstboot
     ./kernel
